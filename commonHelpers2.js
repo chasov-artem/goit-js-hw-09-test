6bce4b8e97ec20796f5279e6db49bd7692493315
chasov-artem <chasov90@gmail.com>
2024-07-11 15:54:13 +0000

    Deploying to gh-pages from @ chasov-artem/goit-js-hw-09@0f149e4d5fd43c1b9f4f89ac037a2857c79c73d6 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const a="feedback-form-state",e=document.querySelector(".feedback-form"),r=e.querySelector("#emailInput");e.querySelector("#textArea");r.addEventListener("input",n);function n(t){const o=t.target.value;localStorage.setItem(a,o)}
+//# sourceMappingURL=commonHelpers2.js.map
